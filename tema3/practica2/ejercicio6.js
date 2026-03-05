260cd7111b94f38bf29d982123116df53651b4f6
balbasMarcos <mabaco890@iesluisbraille.es>
2026-03-05 13:50:35 +0100

    5-03-26

--- tema3/practica2/ejercicio6.js
+++ tema3/practica2/ejercicio6.js
@@ -1,4 +1,4 @@
 const prompt = require('prompt-sync')();
-let texto = prompt('Introduce tu nombre y apellidos separados por espacios: ').toLowerCase().replace(" ",".").replace(" ",".")+"@al.colegiosangabriel.com";
-
+let texto = prompt('Introduce tu nombre y apellidos separados por espacios: ');
+texto = texto.toLowerCase().replace(/ /g, ".")+"@al.colegiosangabriel.com";
 console.log("Tu correo electronico es " + texto);
